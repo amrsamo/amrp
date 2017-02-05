19fc2cc369fef8158133bebd83ab8a7b05134854
Amr Samo <amrsamo75@gmail.com>
2017-02-05 16:14:27 +0200

    mail server

--- contact_me.php
+++ contact_me.php
@@ -41,12 +41,11 @@ $conn->close();
 
 
    
-// Create the email and send the message
-// $to = 'amrsamo75@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-// $email_subject = "Website Contact Form:  $name";
-// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-// $headers .= "Reply-To: $email_address";   
+$to = 'amrsamo75@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
 // mail($to,$email_subject,$email_body,$headers);
 
 
@@ -92,10 +91,11 @@ $mail->Subject = $subject;
 $mail->msgHTML($message);
 
 if (!$mail->send()) {
-   
+   $error = "Mailer Error: " . $mail->ErrorInfo;
+   echo $error;
 } 
 else {
-   
+   echo 'yes';
 }
 
 
